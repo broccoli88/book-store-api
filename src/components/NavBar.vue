26346c9909d41f109430c38f8715119324e9be9b
broccoli88 <kode.kudaki@gmail.com>
2022-06-17 21:04:32 +0200

    part of mobile navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,141 @@
+<template>
+    <header class="flex">
+        <svg
+            class="menu-button"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="button"
+            width="48"
+            height="48"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 32 32"
+        >
+            <path
+                fill="var(--color-secondary-dark)"
+                d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z"
+            />
+            <path
+                fill="var(--color-secondary-dark)"
+                d="M16 10a6 6 0 1 0 6 6a6 6 0 0 0-6-6Z"
+            />
+        </svg>
+        <div class="branding">Books</div>
+        <nav class="flex-col">
+            <h3 class="heading">My library</h3>
+            <ul class="nav-links flex-col">
+                <li class="link"><a href="#">Favourites</a></li>
+                <li class="link"><a href="#">Reading now</a></li>
+                <li class="link"><a href="#">To Read</a></li>
+                <li class="link"><a href="#"> Have Read</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+header {
+    width: 100vw;
+    height: 6rem;
+
+    padding: 1.5rem 3rem 2rem;
+
+    background-color: white;
+
+    justify-content: space-between;
+
+    position: fixed;
+}
+
+.menu-button {
+    position: absolute;
+    top: 0.9rem;
+    right: 3rem;
+
+    cursor: pointer;
+}
+
+.branding {
+    font-size: 2rem;
+    font-weight: 900;
+    text-transform: uppercase;
+
+    color: var(--color-secondary-dark);
+}
+
+nav {
+    width: 60%;
+    height: calc(100vh - 6rem);
+
+    border-left: 3px solid var(--color-secondary-dark);
+    box-shadow: 0 4px 4px hsl(0, 0%, 50%, 0.5);
+    background-color: white;
+
+    justify-content: center;
+    align-items: flex-start;
+    gap: 6rem;
+
+    position: absolute;
+    top: 6rem;
+    right: 0;
+    z-index: 900;
+}
+
+.heading {
+    padding-left: 1rem;
+}
+
+.nav-links {
+    list-style-type: none;
+    width: 100%;
+
+    z-index: 920;
+}
+
+.link {
+    padding: 1rem 0 1rem 1rem;
+    cursor: pointer;
+    position: relative;
+
+    background-color: transparent;
+
+    z-index: 940;
+}
+
+.link > a {
+    position: relative;
+    z-index: 950;
+
+    transition: color 0.6s ease;
+}
+
+.link::before {
+    content: "";
+    display: inline-block;
+
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 930;
+
+    transform: scaleX(0);
+    transform-origin: left;
+
+    background-color: var(--color-secondary-dark);
+    transition: all 0.3s linear;
+}
+
+.link:hover {
+    color: white;
+    font-weight: 600;
+}
+
+.link:hover.link::before {
+    transform: scaleX(1);
+    color: white;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,26 +1,27 @@
 <template>
     <div class="wrapper">
-        <div class="api-content">
-            <span class="api-x"> </span>
-            <span class="api-x"></span>
-            <span class="api-x"></span>
-            <span class="api-x"></span>
-            <span class="api-x"></span>
-        </div>
+        <NavBar />
     </div>
 </template>
 
 <script setup>
-const searchTerm = "jacek piekara";
-const getData = async () => {
-    const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
-    );
-    const data = await response.json();
+// const searchTerm = "jacek piekara";
+// let title = ref("");
+// let author = ref("");
 
-    console.log(data.items[0].volumeInfo);
-};
-getData();
+// const getData = async () => {
+//     const response = await fetch(
+//         `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+//     );
+
+//     const data = await response.json();
+
+//     title.value = data.items[0].volumeInfo.title;
+//     author.value = data.items[0].volumeInfo.authors[0];
+// };
+// getData();
+import NavBar from "../components/NavBar.vue";
+import { ref } from "@vue/reactivity";
 </script>
 
 <style scoped>
@@ -28,9 +29,22 @@ getData();
     width: 100vw;
     min-height: 100vh;
 
-    display: flex;
+    /* display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: center; */
 }
+
+/* .api-content {
+    width: 40vw;
+    height: 30vh;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    justify-content: center;
+    align-items: center;
+
+    background-color: var(--color-primary-light);
+} */
 </style>
